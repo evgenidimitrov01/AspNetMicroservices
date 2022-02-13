dd6fc64d79df613b55a4d33534c30a9b8f73fbaf
Evgeni Dimitrov <sl-edimitrov@allshare.net>
2022-02-13 12:01:02 +0200

    Clean unnecessary components, add abstractions and models

---
+++ src/Services/Catalog/Catalog.API/Constants/ApplicationConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.API.Constants
+{
+    public static class ApplicationConstants
+    {
+        public const string ConnectionString = "DatabaseSettings:ConnectionString";
+        
+        public const string DatabaseName = "DatabaseSettings:DatabaseName";
+
+        public const string CollectionName = "DatabaseSettings:CollectionName";
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Constants;
+using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>(ApplicationConstants.ConnectionString));
+            var database = client.GetDatabase(configuration.GetValue<string>(ApplicationConstants.DatabaseName));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>(ApplicationConstants.CollectionName));
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
