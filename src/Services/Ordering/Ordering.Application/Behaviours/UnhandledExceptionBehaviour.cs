da8eb0dfb6a3b32973865364b20bafc7f4f9cc55
Evgeni Dimitrov <sl-edimitrov@allshare.net>
2022-03-06 22:29:00 +0200

    Develop application behaviours in Ordering.Application

---
+++ src/Services/Ordering/Ordering.Application/Behaviours/UnhandledExceptionBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Ordering.Application.Behaviours
+{
+    public class UnhandledExceptionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger _logger;
+
+        public UnhandledExceptionBehaviour(ILogger<TRequest> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<TResponse> Handle(
+            TRequest request,
+            CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> next)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError(ex, "Application Request: Unhandled Exception for Request {Name} {Request}",
+                    requestName, request);
+
+                throw;
+            }
+        }
+    }
+}
