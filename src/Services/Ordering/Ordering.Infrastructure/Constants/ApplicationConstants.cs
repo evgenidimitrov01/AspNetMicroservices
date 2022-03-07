029efd92bd03605c0322bf6abaf40719c3d4a78d
Evgeni Dimitrov <sl-edimitrov@allshare.net>
2022-03-07 22:15:51 +0200

    Add ApplicationConstatns to Ordering.Infrastructure and update Ordering.API

--- src/Services/Ordering/Ordering.Infrastructure/Constants/ApplicationConstants.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Constants/ApplicationConstants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ordering.Infrastructure.Constants
+﻿namespace Ordering.Infrastructure.Constants
 {
     public static class ApplicationConstants
     {
